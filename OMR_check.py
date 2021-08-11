003a7b33ccf9bdffc6db75f871bd7bafaf8e06be
Debalay-dasgupta <88347086+Debalay-dasgupta@users.noreply.github.com>
2021-08-11 11:19:32 +0530

    Capture answer script, resizing and view

---
+++ OMR_check.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+############################################
+path = "1.jpg"
+widthImg = 700
+heightImg = 700
+
+img = cv2.imread(path)
+
+#big so rescaling it
+
+img = cv2.resize(img,(widthImg,heightImg))
+
+
+cv2.imshow("Answer Script",img)
+
+cv2.waitKey(0)
