d606f4def49b22d0d0c3ad754d5181d4ac66bba7
Debalay-dasgupta <88347086+Debalay-dasgupta@users.noreply.github.com>
2021-08-12 02:52:53 +0530

    contours list,largest and second largest(mcq and grade view)

--- OMR_check.py
+++ OMR_check.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from numpy.lib import utils
 import utlis
 ############################################
 path = "1.jpg"
@@ -14,6 +15,8 @@ img = cv2.imread(path)
 
 img = cv2.resize(img,(widthImg,heightImg))
 imgContours = img.copy()
+imgBiggestContours = img.copy()
+imgGradePoints = img.copy()
 #grayscale preprocessing
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 #add blur to gray image
@@ -26,10 +29,39 @@ imgCanny = cv2.Canny(imgBlur,10,50) #sample threshold
 contours, hierarchy = cv2.findContours(imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) #outeredges,no approximation
 
 cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10) # DRAW ALL DETECTED CONTOURS, copied image,all of them,green selected,thickness
-       
+
+
+
+#finding rectangles
+#reCton is in decreasing order
+
+rectCon = utlis.rectContour(contours)
+biggestContour = rectCon[0]
+#print(len(biggestContour)) #but we need only the corner points
+
+
+#corner points 
+#where mcq present
+biggestContour = utlis.getCornerPoints(rectCon[0])
+print(biggestContour)
+
+#where grade box present probably second largest
+gradePoints = utlis.getCornerPoints(rectCon[1])
+print(gradePoints)
+
+#to check whether desired rectangle is derived
+
+if biggestContour.size !=0 and gradePoints.size != 0:
+    cv2.drawContours(imgBiggestContours,biggestContour,-1,(0,255,0),20)
+    cv2.drawContours(imgGradePoints,gradePoints,-1,(255,0,0),20) #more thick
+    
+
+
+
 #stacking all images and display
 imgBlank = np.zeros_like(img)
-imageArray = ([img,imgGray,imgBlur,imgCanny],[imgContours,imgBlank,imgBlank,imgBlank])
+
+imageArray = ([img,imgGray,imgBlur,imgCanny],[imgContours,imgBiggestContours,imgGradePoints,imgBlank])
 
 imagStacked = utlis.stackImages(imageArray,0.5)#scale given, labels not given
 
